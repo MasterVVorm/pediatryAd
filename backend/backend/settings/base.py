0f37addc18e3289471f3448645b735545a2ff835
Mark <mvincha404@gmail.com>
2019-10-08 14:58:03 +0300

    fixed images serving, deleted package-lock.json

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+k2qc$*mq2sy1j@_gzp&$uylnpt#bpfyj@ghx&8-yckhll_fmo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf.urls import url
 from backend.settings.base import API_VERSION, DEBUG, MEDIA_ROOT, MEDIA_URL
-from django.conf.urls.static import static
-
+from django.conf.urls.static import static, serve
 admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.apps.user.urls')),
     path('api/', include('backend.apps.advertisements.urls')),
-    path('api/version', lambda req: JsonResponse({"version": API_VERSION}))
+    path('api/version', lambda req: JsonResponse({"version": API_VERSION})),
+    path(r'api/media/', serve,
+        {'document_root': MEDIA_ROOT}),
 ]
 
-urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+# urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
